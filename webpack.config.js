a03a0482e7940d48132f87e1bb0f7186f86755bb
Lukas Hermann <lukher6@web.de>
2017-10-24 15:42:20 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./source/_webpack/main.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "js")
+    },
+    devtool: "source-map"
+};
