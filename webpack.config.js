24291b99de9ba5894eef0582c99f2fc8fa0bc292
Lukas Hermann <lukher6@web.de>
2018-10-15 17:01:42 +0800

    Upgrade build pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,53 @@
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const path = require("path");
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
-    entry: "./_webpack/main.js",
+    mode: devMode ? "development" : "production",
+    entry: {
+        main: ["./_webpack/main.js", "./_sass/main.scss"],
+        "editor-style": "./_sass/editor-style.scss"
+    },
     output: {
-        filename: "bundle.js",
+        filename: "[name].js",
         path: path.resolve(__dirname, "dist/js")
     },
-    devtool: "source-map"
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: devMode
+                    ? [
+                          MiniCssExtractPlugin.loader,
+                          "css-loader", // translates CSS into CommonJS
+                          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                      ]
+                    : [
+                          MiniCssExtractPlugin.loader,
+                          "css-loader", // translates CSS into CommonJS
+                          "postcss-loader", // applies autoprefixer to CSS
+                          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                      ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "../css/[name].css",
+            chunkFilename: "../css/[id].css"
+        })
+    ],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true // set to true if you want JS source maps
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    }
 };
